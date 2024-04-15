741189423761fbccef267c4adc732c70a772a4c0
arthur <arthur.bougeard@student-cs.fr>
2024-04-15 14:32:17 +0200

    add JSON display

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import './App.css';
 import '@blocknote/core/fonts/inter.css';
 import { BlockNoteView, useCreateBlockNote } from '@blocknote/react';
 import '@blocknote/react/style.css';
+import { useState } from "react";
+import { Block } from "@blocknote/core";
+
 
 function App() {
+    const [blocks, setBlocks] = useState<Block[]>([]);
+
 	const editor = useCreateBlockNote();
 
-	return <BlockNoteView editor={editor} sideMenu={true} />;
+	return (
+    <div>
+    <div>
+        <BlockNoteView 
+            editor={editor}
+            sideMenu={true} 
+            onChange={() => {
+                // Saves the document JSON to state.
+                setBlocks(editor.document);}}
+        />;
+    </div>
+    <div>Document JSON:</div>
+    <div className={"item bordered"}>
+        <pre>
+            <code>{JSON.stringify(blocks, null, 2)}</code>
+        </pre>
+    </div>
+    </div>)
 }
 
 export default App;
