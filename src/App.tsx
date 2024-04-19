8cf2ca74af20ae7c6a25238fe8ecf413e2798484
Julien Dupont <julien.dupont@student-cs.fr>
2024-04-19 15:30:12 +0200

    feat(translation): demo translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,86 @@
-import './App.css';
+//import './App.css';
 import '@blocknote/core/fonts/inter.css';
 import { BlockNoteView, useCreateBlockNote } from '@blocknote/react';
 import '@blocknote/react/style.css';
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Block } from '@blocknote/core';
+import Progress from './components/Progress';
 
 function App() {
+	const [ready, setReady] = useState<null | boolean>(null);
+	const [disabled, setDisabled] = useState(false);
+	const [progressItems, setProgressItems] = useState([]);
 	const [blocks, setBlocks] = useState<Block[]>([]);
+
+	const [input, setInput] = useState('I love walking my dog.');
+	const [output, setOutput] = useState('');
 
 	const editor = useCreateBlockNote();
 
+	const worker = useRef(null);
+
+	useEffect(() => {
+		if (!worker.current) {
+			worker.current = new Worker(new URL('./worker.js', import.meta.url), {
+				type: 'module',
+			});
+		}
+
+		const onMessageReceived = (e) => {
+			switch (e.data.status) {
+				case 'initiate':
+					setReady(false);
+					setProgressItems((prev) => [...prev, e.data]);
+					break;
+
+				case 'progress':
+					setProgressItems((prev) =>
+						prev.map((item) => {
+							if (item.file === e.data.file) {
+								return { ...item, progress: e.data.progress };
+							}
+							return item;
+						})
+					);
+					break;
+
+				case 'done':
+					setProgressItems((prev) =>
+						prev.filter((item) => item.file !== e.data.file)
+					);
+					break;
+
+				case 'ready':
+					setReady(true);
+					break;
+
+				case 'update':
+					setOutput(e.data.output);
+					break;
+
+				case 'complete':
+					setDisabled(false);
+					break;
+			}
+		};
+
+		worker.current.addEventListener('message', onMessageReceived);
+
+		return () =>
+			worker.current.removeEventListener('message', onMessageReceived);
+	});
+
+	const translate = () => {
+		setDisabled(true);
+		const input = transformateurJsonToString(blocks);
+		console.log(input);
+		worker.current.postMessage({
+			text: input[0],
+		});
+	};
+
 	const transformateurJsonToString = (block) => {
-		let string = [];
-		console.log(block);
+		const string = [];
 		for (let i = 0; i < block.length; i++) {
 			let paragraph = '';
 			if (block[i].type === 'table') {
@@ -47,14 +115,17 @@ function App() {
 					}}
 				/>
 			</div>
-			{transformateurJsonToString(blocks).map((item, index) => (
-				<p key={index}>{item}</p>
-			))}
-			<div>Document JSON:</div>
-			<div className={'item bordered'}>
-				<pre>
-					<code>{JSON.stringify(blocks, null, 2)}</code>
-				</pre>
+			<button disabled={disabled} onClick={translate}>
+				Translate
+			</button>
+			<div>{output}</div>
+			<div className='progress-bars-container'>
+				{ready === false && <label>Loading models... (only run once)</label>}
+				{progressItems.map((data) => (
+					<div key={data.file}>
+						<Progress text={data.file} percentage={data.progress} />
+					</div>
+				))}
 			</div>
 		</div>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+//import './index.css';
 import Transformers from './Transformers.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Transformers />
+		<App />
+		{/*<Transformers />*/}
 	</React.StrictMode>
 );
