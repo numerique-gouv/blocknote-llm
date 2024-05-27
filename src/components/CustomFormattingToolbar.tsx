12f9c03d539cca47bbafac3d51ebbd64c1271e2a
arthur <arthur.bougeard@student-cs.fr>
2024-05-27 16:54:48 +0200

    new correction design

--- src/components/CustomFormattingToolbar.tsx
+++ src/components/CustomFormattingToolbar.tsx
@@ -1,15 +1,16 @@
 import { BasicTextStyleButton, BlockTypeSelect, ColorStyleButton, CreateLinkButton, FormattingToolbar, ImageCaptionButton, NestBlockButton, ReplaceImageButton, TextAlignButton, UnnestBlockButton } from "@blocknote/react";
 import { TranslateToolbarButton } from "./TranslateToolbarButton";
 import { CorrectToolbarButton } from "./CorrectToolbarButton";
+import { EngineInterface } from "@mlc-ai/web-llm";
 
-export function CustomFormattingToolbar({ onSend }) {
+export function CustomFormattingToolbar({ onSend, engine }: { onSend: () => void, engine: EngineInterface}) {
     return (
         <FormattingToolbar>
             <BlockTypeSelect key={"blockTypeSelect"} />
     
             {/* Extra button to toggle blue text & background */}
-            <TranslateToolbarButton key={"translateButton"} onSend={onSend}/>
-            <CorrectToolbarButton key={"correctButton"} onSend={onSend}/>
+            <TranslateToolbarButton key={"translateButton"} onSend={onSend} engine={engine}/>
+            <CorrectToolbarButton key={"correctButton"} onSend={onSend} engine={engine}/>
     
             <ImageCaptionButton key={"imageCaptionButton"} />
             <ReplaceImageButton key={"replaceImageButton"} />
--- src/utils/diffText.ts
+++ src/utils/diffText.ts
@@ -1,59 +1,95 @@
 import { StyleSchema, StyledText } from '@blocknote/core';
+import { diffWords } from 'diff'
 
 function diffText(
 	originalText: string,
-	correctedText: string
+	correctedText: string,
+    design: number = 2
 ): [StyledText<StyleSchema>[], StyledText<StyleSchema>[]] {
 	const originalWords = originalText.split(/[   ]/);
 	const correctedWords = correctedText.split(/[   ]/);
 	const formattedSourceContent: StyledText<StyleSchema>[] = [];
 	const formattedCorrectedContent: StyledText<StyleSchema>[] = [];
-	for (
-		let index = 0;
-		index < correctedWords.length && index < originalWords.length;
-		index++
-	) {
-		const correctedWord = correctedWords[index];
-		const originalWord = originalWords[index];
-		if (correctedWord === originalWord) {
-			formattedSourceContent.push({
-				type: 'text',
-				text: originalWord + ' ',
-				styles: {},
-			});
-			formattedCorrectedContent.push({
-				type: 'text',
-				text: originalWord + ' ',
-				styles: {},
-			});
-		} else {
-			formattedSourceContent.push({
-				type: 'text',
-				text: originalWord + ' ',
-				styles: { backgroundColor: 'red' },
-			});
-			formattedCorrectedContent.push({
-				type: 'text',
-				text: correctedWord + ' ',
-				styles: { backgroundColor: 'red' },
-			});
-		}
-	}
 
-	for (
-		let index = correctedWords.length;
-		index < originalWords.length;
-		index++
-	) {
-		const word = originalWords[index];
-		formattedSourceContent.push({
-			type: 'text',
-			text: word + ' ',
-			styles: {},
-		});
-	}
+    if (design == 1) {
+        for (
+            let index = 0;
+            index < correctedWords.length && index < originalWords.length;
+            index++
+        ) {
+            const correctedWord = correctedWords[index];
+            const originalWord = originalWords[index];
+            if (correctedWord === originalWord) {
+                formattedSourceContent.push({
+                    type: 'text',
+                    text: originalWord + ' ',
+                    styles: {},
+                });
+                formattedCorrectedContent.push({
+                    type: 'text',
+                    text: originalWord + ' ',
+                    styles: {},
+                });
+            } else {
+                formattedSourceContent.push({
+                    type: 'text',
+                    text: originalWord + ' ',
+                    styles: { backgroundColor: 'red' },
+                });
+                formattedCorrectedContent.push({
+                    type: 'text',
+                    text: correctedWord + ' ',
+                    styles: { backgroundColor: 'red' },
+                });
+            }
+        }
 
-	return [formattedSourceContent, formattedCorrectedContent];
+        for (
+            let index = correctedWords.length;
+            index < originalWords.length;
+            index++
+        ) {
+            const word = originalWords[index];
+            formattedSourceContent.push({
+                type: 'text',
+                text: word + ' ',
+                styles: {},
+            });
+        }
+
+	    return [formattedSourceContent, formattedCorrectedContent];
+    } else {
+        const diff = diffWords(originalText, correctedText)
+        let sourceContent = []
+        let correctedContent = []
+        for (const sequence of diff) {
+            if (sequence.added) {
+                correctedContent.push({
+                    type: 'text',
+                    text: sequence.value,
+                    styles: { backgroundColor: 'green'}
+                })
+            } else if (sequence.removed) {
+                sourceContent.push({
+                    type: 'text',
+                    text: sequence.value,
+                    styles: { backgroundColor: 'red'}
+                })
+            } else {
+                correctedContent.push({
+                    type: 'text',
+                    text: sequence.value,
+                    styles: {}
+                })
+                sourceContent.push({
+                    type: 'text',
+                    text: sequence.value,
+                    styles: {}
+                })
+            }
+        }
+        return [sourceContent, correctedContent]
+    }
 }
 
 export default diffText;
