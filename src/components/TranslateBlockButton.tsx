c7da90ad54411d75a4ae3c12992bb4fbb22f704b
arthur <arthur.bougeard@student-cs.fr>
2024-05-31 17:20:06 +0200

    delete useless parser in TranslateBlockButton.tsx

--- src/components/TranslateBlockButton.tsx
+++ src/components/TranslateBlockButton.tsx
@@ -1,6 +1,7 @@
 import { Block } from '@blocknote/core';
 import { SideMenuButton, SideMenuProps } from '@blocknote/react';
 import { SiGoogletranslate } from 'react-icons/si';
+import { transformateurJsonToString } from '../utils/ParserBlockToString';
 
 interface TranslateBlockButtonProps extends SideMenuProps {
 	worker: React.MutableRefObject<Worker | null>;
@@ -8,26 +9,6 @@ interface TranslateBlockButtonProps extends SideMenuProps {
 
 const TranslateBlockButton: React.FC<TranslateBlockButtonProps> = (props) => {
 	const { worker } = props;
-
-	const transformateurJsonToString = (block: Block) => {
-		let text = '';
-		if (block.type === 'table') {
-			for (let i = 0; i < block.content.rows.length; i++) {
-				for (let j = 0; j < block.content.rows[i].cells.length; j++) {
-					for (let k = 0; k < block.content.rows[i].cells[j].length; k++) {
-						text += ' ';
-						text += block.content.rows[i].cells[j][k]?.text;
-					}
-				}
-			}
-		} else if (block.content) {
-			for (let i = 0; i < block.content.length; i++) {
-				text += block.content[i]?.text;
-			}
-		}
-
-		return text;
-	};
 
 	const translate = () => {
 		worker.current?.postMessage({
